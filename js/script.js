95e742bdcbe951cfa26d5970b3a9fc57304337bf
pixma38@gmail.com <pixma38@gmail.ccom>
2016-08-22 02:55:08 +0530

    Blog Updated and on onwards.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(function(){
+
+});
+
+$('#button-instagram').on('click', function(event) {
+	
+	/* Act on the event */
+	showNotification('bg-black',"Follow me @annimbanerjee", 'top', 'right', 'animated bounceInRight', 'animated bounceOutRight');
+});
